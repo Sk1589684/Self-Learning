fc66e0df410548776bb735eb5216038687420acb
Sk1589684 <sarveshkulkarni.2003@gmail.com>
2026-03-09 00:43:45 +0530

    Add files via upload

---
+++ rd.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int id[30];
+	char name[30];
+	int i,j;
+	for(i=0;i<30;i++)
+	{
+		cout<<"enter the id";
+		cin>>id[i];
+			
+			for(j=0;j<i;j++)
+			{
+				cout<<"enter the name";
+				cin>>name[j];
+		
+			}
+	
+	
+		
+		
+		
+	}
+	cout<<"the is of employee"<<id<<endl;
+		cout<<"the name of employee"<<name<<endl;
+
+}
+
+
